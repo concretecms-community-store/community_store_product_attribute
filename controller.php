1ce19a70d87097ccb079a676b1147e66c0729379
Ryan Hewitt <ryan@mesuva.com.au>
2020-12-30 12:31:43 +0000

    Remove legacy way of checking for install dependancies, replace with $packageDependancies

--- controller.php
+++ controller.php
@@ -2,13 +2,13 @@
 namespace Concrete\Package\CommunityStoreProductAttribute;
 
 use Concrete\Core\Package\Package;
-use Whoops\Exception\ErrorException;
 
 class Controller extends Package
 {
     protected $pkgHandle = 'community_store_product_attribute';
     protected $appVersionRequired = '8.0';
     protected $pkgVersion = '0.5';
+    protected $packageDependencies = ['community_store'=>'2.0'];
 
     public function getPackageDescription()
     {
@@ -22,17 +22,11 @@ class Controller extends Package
 
     public function install()
     {
-        $installed = $this->app->make('Concrete\Core\Package\PackageService')->getInstalledHandles();
-        if(!(is_array($installed) && in_array('community_store',$installed)) ) {
-            throw new ErrorException(t('This package requires that Community Store be installed'));
-        } else {
-            $pkg = parent::install();
-            $factory = $this->app->make('Concrete\Core\Attribute\TypeFactory');
-            $type = $factory->getByHandle('community_store_product');
-            if (!is_object($type)) {
-                $type = $factory->add('community_store_product', 'Product', $pkg);
-            }
+        $pkg = parent::install();
+        $factory = $this->app->make('Concrete\Core\Attribute\TypeFactory');
+        $type = $factory->getByHandle('community_store_product');
+        if (!is_object($type)) {
+            $type = $factory->add('community_store_product', 'Product', $pkg);
         }
-
     }
 }
